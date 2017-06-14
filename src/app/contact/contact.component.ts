2a867df0d51a75db8bc8b2d346b979381986fe25
Cai Chengyuan (XLD5QFR) <ccai41@gatech.edu>
2017-06-14 15:35:21 -0400

    add simple validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import CustomValidators from '../forms/CustomValidators';
 
 @Component({
@@ -9,16 +9,18 @@ import CustomValidators from '../forms/CustomValidators';
 })
 export class ContactComponent implements OnInit {
   contactForm: FormGroup;
+  
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, CustomValidators.validateEmail]],
-      content: ['', [Validators.required, Validators.minLength(10)]]
+      email: ['', Validators.required],
+      phone: '',
+      message: ['', [Validators.required, Validators.minLength(10)]]
     });
   }
-  submitForm(): void {
-    console.log(this.contactForm);
+  onContact() {
+    console.log(this.contactForm.value);
   }
 }
